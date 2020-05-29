e7fb6c2339825dfea6b01f7c0d3a445baabd5f5e
Sagar <57942799+poudel-sagar@users.noreply.github.com>
2020-05-29 07:03:39 +0545

    Create today.java

---
+++ today.java
@@ -0,0 +1,30 @@
+ bank_Card.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent bankCardd= new Intent(MainActivity.this, scienceOne.class);
+                startActivity(bankCardd);
+            }
+        });
+
+
+        ideas_Card.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent ideasCardd= new Intent(MainActivity.this, cmathOne.class);
+                startActivity(ideasCardd);
+            }
+        });
+        add_Card.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent addCardd  = new Intent(MainActivity.this, omathOne.class);
+                startActivity(addCardd);
+            }
+        });
+        link_Card.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent linkCardd= new Intent(MainActivity.this, gamesOne.class);
+                startActivity(linkCardd);
+            }
+        });
